da2a9c9c81d6b5eddc7e0fdd328abef582995ec1
Mike Manzo <manzo.mike@gmail.com>
2019-02-23 11:10:05 -0500

    Added MacToggle

--- MeteoBar/Utility/QLExtensions.swift
+++ MeteoBar/Utility/QLExtensions.swift
@@ -152,6 +152,19 @@ extension NSTextField {
     }
 }
 
+extension NSView {
+    var backgroundColor: NSColor? {
+        get {
+            guard let layer = layer, let backgroundColor = layer.backgroundColor else { return nil }
+            return NSColor(cgColor: backgroundColor)
+        }
+        set {
+            wantsLayer = true
+            layer?.backgroundColor = newValue?.cgColor
+        }
+    }
+}
+
 extension Optional {
     public var value: String {
         switch self {
